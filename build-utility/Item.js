e31afe83f068e08c1d158bb36d44e645fe1811f1
Philippe Leclerc <philippeleclerc299@gmail.com>
2018-10-21 20:52:50 -0400

    Implemented parsing for Disgaea items

---
+++ build-utility/Item.js
@@ -0,0 +1,79 @@
+class Item {
+    constructor(data) {
+        try {
+            this.jpName = data[Indexes.JAPANESE_NAME];
+            this.name = data[Indexes.ENGLISH_NAME];
+
+            this.itemRank = data[Indexes.ITEM_RANK];
+            this.special = this.itemRank === "-";
+
+            this.shopLevel = data[Indexes.SHOP_LEVEL];
+            if (isNaN(this.shopLevel)) {
+                this.shopCost = "-";
+            } else {
+                this.shopCost = data[Indexes.SHOP_COST];
+            }
+
+            this.hp = validateNumber(data[Indexes.HP]);
+            this.sp = validateNumber(data[Indexes.SP]);
+            this.atk = validateNumber(data[Indexes.ATK]);
+            this.def = validateNumber(data[Indexes.DEF]);
+            this.int = validateNumber(data[Indexes.INT]);
+            this.res = validateNumber(data[Indexes.RES]);
+            this.hit = validateNumber(data[Indexes.HIT]);
+            this.spd = validateNumber(data[Indexes.SPD]);
+
+            this.mouvement = validateNumber(data[Indexes.MOVEMENT]);
+            this.jump = validateNumber(data[Indexes.JUMP]);
+            this.range = validateNumber(data[Indexes.RANGE]);
+            this.ct = validateNumber(data[Indexes.CT]);
+            this.crit = validateNumber(data[Indexes.CRITICAL]);
+
+            this.innocent = data[Indexes.INNOCENT];
+        } catch (rootError) {
+            throw new Error(`Could not create item with data,\n Data : ${data}\n Cause : ${rootError}`);
+        }
+    }
+
+    isSpecial() {
+        return this.special;
+    }
+}
+
+function validateNumber(input) {
+    if (input === '') {
+        return 0;
+    } else {
+        if (isNaN(input)) {
+            throw new Error(`Unexpected string ["${input}"] when parsing int value...`);
+        }
+        return parseInt(input);
+    }
+}
+
+const Indexes = {
+    JAPANESE_NAME: 0,
+    ENGLISH_NAME: 1,
+    ITEM_RANK: 2,
+    SHOP_LEVEL: 3,
+    SHOP_COST: 4,
+    HP: 5,
+    SP: 6,
+    ATK: 7,
+    DEF: 8,
+    INT: 9,
+    RES: 10,
+    HIT: 11,
+    SPD: 12,
+    MOVEMENT: 13,
+    JUMP: 14,
+    RANGE: 15,
+    CT: 16,
+    CRITICAL: 17,
+    INNOCENT: 18
+};
+
+module.exports = {
+    Item,
+    Indexes
+};
---
+++ build-utility/dataExtractor.js
@@ -0,0 +1,82 @@
+const csv = require('csv-parse');
+const fs = require('fs');
+const {Item, Indexes} = require('./Item');
+
+const EXPECTED_COLUMN = 19;
+
+const bundleLocation = `${__dirname}/../src/assets/items.json`;
+
+const basePath = `${__dirname}/data`;
+const GamePaths = {
+    DISGAEA_5: "disgaea-5"
+};
+const itemDirectory = "items";
+
+const shortCutVariable = `${basePath}/${GamePaths.DISGAEA_5}/${itemDirectory}`;
+
+const ItemFiles = {
+    FIST: `${shortCutVariable}/Disgaea 5 Lists - Fist.csv`,
+    SWORD: `${shortCutVariable}/Disgaea 5 Lists - Sword.csv`,
+    SPEAR: `${shortCutVariable}/Disgaea 5 Lists - Spear.csv`,
+    BOW: `${shortCutVariable}/Disgaea 5 Lists - Gun.csv`,
+    AXE: `${shortCutVariable}/Disgaea 5 Lists - Axe.csv`,
+    STAFF: `${shortCutVariable}/Disgaea 5 Lists - Staff.csv`,
+    MONSTER_ATK: `${shortCutVariable}/Disgaea 5 Lists - MATK.csv`,
+};
+
+const items = {
+    FIST: [],
+    SWORD: [],
+    SPEAR: [],
+    BOW: [],
+    AXE: [],
+    STAFF: [],
+    MONSTER_ATK: [],
+    [Symbol.iterator]: function* () {
+        for (let key in items) {
+            yield items[key];
+        }
+    }
+};
+
+function isParsedDataValid(parsedData) {
+    // Arbitrary check on atk index, not the best, but working for now and I dont feel like adding a
+    // complicated logic for it ...
+    // todo: Implement better logic to make sure the data is insert-able as an Item
+    return parsedData.length === EXPECTED_COLUMN && !isNaN(parsedData[Indexes.ATK]);
+}
+
+async function parseFile(file) {
+    const container = [];
+
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(file)
+            .pipe(csv())
+            .on('data', (line) => {
+                if (isParsedDataValid(line)) {
+                    try {
+                        container.push(new Item(line));
+                    } catch (error) {
+                        throw new Error(`Error when parsin file [${file}]\n${error}`)
+                    }
+                }
+            })
+            .on('error', () => reject(Error(`Could not parse file [${file}]`)))
+            .on('end', () => {
+                resolve(container);
+                return container;
+            });
+    });
+}
+
+async function parseAllItems() {
+    for (let fileKey in ItemFiles) {
+        items[fileKey] = await parseFile(ItemFiles[fileKey]);
+    }
+}
+
+
+parseAllItems().then(async () => {
+    await fs.writeFileSync(bundleLocation, JSON.stringify(items, null, 2), "utf-8");
+    console.log(`Successfully created JSON bundle at [${bundleLocation}]`)
+});
