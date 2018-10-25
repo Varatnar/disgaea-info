bce348e0ba28f3fde20d1f6e460609106e8a449a
Philippe Leclerc <philippeleclerc299@gmail.com>
2018-10-25 15:43:30 -0400

    Added rendering for all skill data

--- src/components/class/skill/SkillComponent.vue
+++ src/components/class/skill/SkillComponent.vue
@@ -3,6 +3,30 @@
         <div class="cell">
             {{skill.name}}
         </div>
+        <div class="cell">
+            {{skill.lvl}}
+        </div>
+        <div class="cell">
+            {{skill.sp}}
+        </div>
+        <div class="cell">
+            {{skill.power}}
+        </div>
+        <div class="cell">
+            {{skill.stat}}
+        </div>
+        <div class="cell">
+            {{skill.element}}
+        </div>
+        <div class="cell">
+            {{skill.ailment}}
+        </div>
+        <div class="cell">
+            {{skill.range}}
+        </div>
+        <div class="cell">
+            {{skill.description}}
+        </div>
     </div>
 </template>
 
--- src/components/class/skill/SkillsComponent.vue
+++ src/components/class/skill/SkillsComponent.vue
@@ -1,6 +1,35 @@
 <template>
     <div class="skills-section">
         <div class="table">
+            <div class="row">
+                <div class="cell cell-column-title">
+                    Name
+                </div>
+                <div class="cell cell-column-title">
+                    Level
+                </div>
+                <div class="cell cell-column-title">
+                    SP
+                </div>
+                <div class="cell cell-column-title">
+                    Power
+                </div>
+                <div class="cell cell-column-title">
+                    Stat
+                </div>
+                <div class="cell cell-column-title">
+                    Element
+                </div>
+                <div class="cell cell-column-title">
+                    Ailment
+                </div>
+                <div class="cell cell-column-title">
+                    Range
+                </div>
+                <div class="cell cell-column-title">
+                    Description
+                </div>
+            </div>
             <SkillComponent v-for="skill in skills" :skill="skill" :key="skill.name"/>
         </div>
     </div>
