0567b5d1f477d142b3c9dda9ea236fe8b9c4a752
Philippe Leclerc <philippeleclerc299@gmail.com>
2018-10-25 21:06:10 -0400

    Switching to fontawesome-vue and implementing basic router alternation from /items to /classes

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,39 @@
 <template>
     <div id="app">
+        <div class="switcher" @click="resolvePageMove()">
+            <font-awesome-icon icon="location-arrow"/>
+        </div>
         <router-view></router-view>
     </div>
 </template>
 
 <script lang="ts">
+    import { library } from "@fortawesome/fontawesome-svg-core";
+    import { faLocationArrow } from "@fortawesome/free-solid-svg-icons";
+    import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
     import Vue from "vue";
     import { Component } from "vue-property-decorator";
     import MainItems from "./views/MainItems.vue";
     import SingleClassJob from "./views/SingleClassJob.vue";
 
+    library.add(faLocationArrow);
+
     @Component({
-        components: {SingleClassJob, MainItems}
+        components: {
+            SingleClassJob,
+            MainItems,
+            FontAwesomeIcon
+        }
     })
     export default class App extends Vue {
+
+        public resolvePageMove() {
+            if (this.$router.currentRoute.fullPath === "/items") {
+                this.$router.push("/classes");
+            } else {
+                this.$router.push("/items");
+            }
+        }
     }
 </script>
 
@@ -51,6 +71,23 @@
 
     #nav a.router-link-exact-active {
         color: #42b983;
+    }
+
+    .switcher {
+        position: absolute;
+        right: 5px;
+
+        padding: 10px;
+
+        border-radius: 5px;
+        border: 1px solid black;
+        background-color: whitesmoke;
+        opacity: 0.2;
+    }
+
+    .switcher:hover {
+        opacity: 0.8;
+        cursor: pointer;
     }
 
     .table {
